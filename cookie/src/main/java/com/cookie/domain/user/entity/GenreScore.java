c14950ddcaa9846ba9e95a222997ac0a3e8627bb
fbgjung <fbgjung@gmail.com>
2024-11-18 22:15:09 +0900

    Refactor: UserCategory 엔티티 삭제 및 GenreScore 엔티티 필드 수정

--- cookie/src/main/java/com/cookie/domain/user/entity/GenreScore.java
+++ cookie/src/main/java/com/cookie/domain/user/entity/GenreScore.java
@@ -20,13 +20,35 @@ public class GenreScore {
     @JoinColumn(name = "user_id")
     private User user;
 
-    private String genreCode;
-    private double genreScore;
+    private double romance;
+    private double horror;
+    private double comedy;
+    private double action;
+    private double fantasy;
+    private double animation;
+    private double crime;
+    private double sf;
+    private double music;
+    private double thriller;
+    private double queer;
+    private double war;
+    private double documentary;
 
     @Builder
-    public GenreScore(User user, String genreCode, double genreScore) {
+    public GenreScore(User user, double romance, double horror, double comedy, double action, double fantasy, double animation, double crime, double sf, double music, double thriller, double queer, double war, double documentary) {
         this.user = user;
-        this.genreCode = genreCode;
-        this.genreScore = genreScore;
+        this.romance = romance;
+        this.horror = horror;
+        this.comedy = comedy;
+        this.action = action;
+        this.fantasy = fantasy;
+        this.animation = animation;
+        this.crime = crime;
+        this.sf = sf;
+        this.music = music;
+        this.thriller = thriller;
+        this.queer = queer;
+        this.war = war;
+        this.documentary = documentary;
     }
 }
