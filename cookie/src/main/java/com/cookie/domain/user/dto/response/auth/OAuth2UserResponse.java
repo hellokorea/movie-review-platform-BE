3c7458459645b70982926b9c3e5f278878eae37b
howecofe <am0229@naver.com>
2024-11-24 02:32:17 +0900

    Refactor: OAuth2UserResponse 필드 추가

--- cookie/src/main/java/com/cookie/domain/user/dto/response/auth/OAuth2UserResponse.java
+++ cookie/src/main/java/com/cookie/domain/user/dto/response/auth/OAuth2UserResponse.java
@@ -1,11 +1,16 @@
 package com.cookie.domain.user.dto.response.auth;
 
+import com.cookie.domain.user.entity.enums.SocialProvider;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class OAuth2UserResponse {
+    private SocialProvider socialProvider;
+    private String email;
+    private String socialId;
     private String role;
     private String nickname;
+    private boolean isRegistrationRequired;
 }
