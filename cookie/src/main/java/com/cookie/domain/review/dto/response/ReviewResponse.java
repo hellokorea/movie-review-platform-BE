8628a4f159ec8a0ce5f2b89640376378f68f2024
fbgjung <fbgjung@gmail.com>
2024-11-24 18:45:34 +0900

    Refactor: 리뷰 데이터 반환값에 유저가 해당 리뷰에 좋아요를 했는지 여부에 대한 필드 추가

--- cookie/src/main/java/com/cookie/domain/review/dto/response/ReviewResponse.java
+++ cookie/src/main/java/com/cookie/domain/review/dto/response/ReviewResponse.java
@@ -25,8 +25,9 @@ public class ReviewResponse {
     private LocalDateTime updatedAt;
     private ReviewMovieResponse movie;
     private ReviewUserResponse user;
+    private boolean likedByUser;
 
-    public static ReviewResponse fromReview(Review review) {
+    public static ReviewResponse fromReview(Review review, boolean likedByUser) {
         return new ReviewResponse(
                 review.getId(),
                 review.getContent(),
@@ -44,7 +45,8 @@ public class ReviewResponse {
                         review.getUser().getNickname(),
                         review.getUser().getProfileImage(),
                         review.getUser().getMainBadge() != null ? review.getUser().getMainBadge().getBadgeImage() : null
-                )
+                ),
+                likedByUser
         );
     }
 
