57d9bfe6868f21db70e7422cb4bbbc96b77478da
howecofe <am0229@naver.com>
2024-11-26 16:54:42 +0900

    Fix: 오류 수정

--- cookie/src/main/java/com/cookie/domain/director/repository/DirectorRepository.java
+++ cookie/src/main/java/com/cookie/domain/director/repository/DirectorRepository.java
@@ -1,6 +1,5 @@
 package com.cookie.domain.director.repository;
 
-import com.cookie.domain.director.dto.response.DirectorResponse;
 import com.cookie.domain.director.entity.Director;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,12 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DirectorRepository extends JpaRepository<Director, Long> {
-    @Query("SELECT new com.cookie.domain.director.dto.response.DirectorResponse("
-            + "d.name, d.profileImage) "
-            + "FROM Director d "
-            + "JOIN MovieDirector md ON md.director.id = d.id "
-            + "WHERE md.movie.id = :movieId")
-    List<DirectorResponse> findDirectorsByMovieId(@Param("movieId") Long movieId);
 
     @Query("""
             SELECT d
