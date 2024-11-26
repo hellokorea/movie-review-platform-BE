02848205d1b8d982aa1d3e000f038af66db6a546
howecofe <am0229@naver.com>
2024-11-26 11:57:48 +0900

    Feat: 검색을 위한 Repository 메서드 구현

--- cookie/src/main/java/com/cookie/domain/actor/repository/ActorRepository.java
+++ cookie/src/main/java/com/cookie/domain/actor/repository/ActorRepository.java
@@ -1,6 +1,8 @@
 package com.cookie.domain.actor.repository;
 
 import com.cookie.domain.actor.entity.Actor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,4 +17,6 @@ public interface ActorRepository extends JpaRepository<Actor, Long> {
             WHERE a.tmdbCasterId =:tmdbCasterId
            """)
     Optional<Actor> findByTMDBCasterId(@Param("tmdbCasterId") Long tmdbCasterId);
+
+    Page<Actor> findByNameContainingIgnoreCase(String keyword, Pageable pageable);
 }
