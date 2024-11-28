3b77b8181d43d65149cc6e219916d37c559c24c3
howecofe <am0229@naver.com>
2024-11-28 15:28:27 +0900

    Feat: 일반 로그인 CustomUserDetails 추가

--- cookie/src/main/java/com/cookie/domain/user/service/CustomUserDetailsService.java
+++ cookie/src/main/java/com/cookie/domain/user/service/CustomUserDetailsService.java
@@ -1,29 +1,32 @@
 package com.cookie.domain.user.service;
 
+import com.cookie.domain.user.dto.response.auth.CustomUserDetails;
 import com.cookie.domain.user.repository.UserRepository;
-import org.springframework.security.core.userdetails.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByNickname(username)
-                .map(user -> User.builder()
-                        .username(user.getNickname())
-                        .password(user.getPassword())
-                        .roles(user.getRole().name())
-                        .build())
+                .map(user -> new CustomUserDetails(
+                        user.getId(),
+                        user.getNickname(),
+                        user.getPassword(),
+                        List.of(new SimpleGrantedAuthority(user.getRole().name()))
+                ))
                 .orElseThrow(() -> new UsernameNotFoundException("NOT_FOUND_USER"));
     }
+
 }
