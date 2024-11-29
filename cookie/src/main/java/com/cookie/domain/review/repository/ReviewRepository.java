c593ab714fa343df00e59dea8fcb0f4bf0202012
fbgjung <fbgjung@gmail.com>
2024-11-29 21:23:09 +0900

    Refactor: 숨김 처리된 리뷰 제외하고 반환하도록 수정

--- cookie/src/main/java/com/cookie/domain/review/repository/ReviewRepository.java
+++ cookie/src/main/java/com/cookie/domain/review/repository/ReviewRepository.java
@@ -17,16 +17,16 @@ import java.util.Optional;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Optional<Review> findByUserAndMovie(User user, Movie movie);
 
-    @Query("SELECT r FROM Review r JOIN FETCH r.movie m JOIN FETCH r.user u LEFT JOIN FETCH u.userBadges ub LEFT JOIN FETCH ub.badge b ORDER BY r.updatedAt DESC")
+    @Query("SELECT r FROM Review r JOIN FETCH r.movie m JOIN FETCH r.user u LEFT JOIN FETCH u.userBadges ub LEFT JOIN FETCH ub.badge WHERE r.isHide = false ORDER BY r.updatedAt DESC")
     Page<Review> findAllWithMovieAndUser(Pageable pageable);
 
-    @Query("SELECT r FROM Review r JOIN FETCH r.movie m JOIN FETCH r.user u LEFT JOIN FETCH u.userBadges ub LEFT JOIN FETCH ub.badge b WHERE r.isSpoiler = true ORDER BY r.updatedAt DESC")
+    @Query("SELECT r FROM Review r JOIN FETCH r.movie m JOIN FETCH r.user u LEFT JOIN FETCH u.userBadges ub LEFT JOIN FETCH ub.badge b WHERE r.isSpoiler = true AND r.isHide = false ORDER BY r.updatedAt DESC")
     Page<Review> findAllWithMovieAndUserWithSpoilers(Pageable pageable);
 
-    @Query("SELECT r FROM Review r JOIN FETCH r.user u LEFT JOIN FETCH u.userBadges ub LEFT JOIN FETCH ub.badge b WHERE r.movie.id = :movieId")
+    @Query("SELECT r FROM Review r JOIN FETCH r.user u LEFT JOIN FETCH u.userBadges ub LEFT JOIN FETCH ub.badge b WHERE r.movie.id = :movieId AND r.isHide = false")
     Page<Review> findReviewsByMovieId(Long movieId, Pageable pageable);
 
-    @Query("SELECT r FROM Review r JOIN FETCH r.user u LEFT JOIN FETCH u.userBadges ub LEFT JOIN FETCH ub.badge b WHERE r.movie.id = :movieId AND r.isSpoiler = true")
+    @Query("SELECT r FROM Review r JOIN FETCH r.user u LEFT JOIN FETCH u.userBadges ub LEFT JOIN FETCH ub.badge b WHERE r.movie.id = :movieId AND r.isSpoiler = true AND r.isHide = false")
     Page<Review> findSpoilerReviewsByMovieId(Long movieId, Pageable pageable);
   
     // 특정 유저의 리뷰와 연관된 영화 정보를 가져오기
