5a36ba5018837910e12e529830735e506c9c12e2
fbgjung <fbgjung@gmail.com>
2024-12-02 23:02:28 +0900

    Test: ReviewService 영화 리뷰 등록 실패 테스트 - MovieNotFound

--- cookie/src/test/java/com/cookie/domain/review/service/ReviewServiceTest.java
+++ cookie/src/test/java/com/cookie/domain/review/service/ReviewServiceTest.java
@@ -20,6 +20,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
@@ -78,6 +79,30 @@ class ReviewServiceTest {
         assertThat(savedReview.getMovieScore()).isEqualTo(createReviewRequest.getMovieScore());
         assertThat(savedReview.getMovie().getTitle()).isEqualTo(movie.getTitle());
 
+    }
+
+
+    @Test
+    @DisplayName("영화 리뷰 등록 실패 테스트 - MovieNotFound")
+    void createReview_fail_MovieNotFound() {
+        Long userId = 1L;
+        Long movieId = 999L;
+
+        CreateReviewRequest createReviewRequest = new CreateReviewRequest(
+                movieId, "재밌었어요!", 4, false
+        );
+
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+        given(movieRepository.findById(movieId)).willReturn(Optional.empty());
+
+        CopyOnWriteArrayList<SseEmitter> reviewEmitters = new CopyOnWriteArrayList<>();
+        CopyOnWriteArrayList<SseEmitter> pushNotificationEmitters = new CopyOnWriteArrayList<>();
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            reviewService.createReview(userId, createReviewRequest, reviewEmitters, pushNotificationEmitters);
+        });
+
+        assertThat(exception.getMessage()).contains("not found movieId");
     }
 
 }
