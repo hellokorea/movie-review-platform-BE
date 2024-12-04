2752cf4348308ff0de53423368e853a531a0b7e2
kjd <wjdehddk123@naver.com>
2024-12-05 03:41:23 +0900

    Feat: #91 Swagger 설정, 컨트롤러 기반 작업

--- cookie/src/main/java/com/cookie/admin/controller/AdminMovieController.java
+++ cookie/src/main/java/com/cookie/admin/controller/AdminMovieController.java
@@ -1,9 +1,18 @@
 package com.cookie.admin.controller;
 
 import com.cookie.admin.dto.response.*;
-import com.cookie.admin.service.movie.*;
+import com.cookie.admin.service.movie.AdminMovieCreateService;
+import com.cookie.admin.service.movie.AdminMovieModifyService;
+import com.cookie.admin.service.movie.AdminMovieReadService;
+import com.cookie.admin.service.movie.AdminMovieSearchService;
 import com.cookie.global.util.ApiUtil;
 import com.cookie.global.util.ApiUtil.ApiSuccess;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Admin 영화 관리", description = "Admin 영화 관리 API")
 @RequestMapping("/api/admin")
 public class AdminMovieController {
 
@@ -20,30 +30,43 @@ public class AdminMovieController {
     private final AdminMovieModifyService adminMovieModifyService;
     private final AdminMovieSearchService adminMovieSearchService;
 
+    @Hidden
     @PostMapping("/movie/base")
     public ApiSuccess<?> defaultMoviesAdd() {
         AdminMovieBaseAddResponse data = adminMovieCreateService.defaultMoviesAdd();
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "TMDB 영화 검색", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdminSearchResponse.class)))
+    })
     @GetMapping("/movie/tmdb/{movieName}")
     public ApiSuccess<?> getMoviesByName(@PathVariable("movieName") String movieName) {
         List<AdminSearchResponse> data = TMDBService.getMoviesByName(movieName);
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "TMDB 검색 된 영화 선택", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdminMovieTMDBDetailResponse.class)))
+    })
     @GetMapping("/movie/tmdb/choice/{movieId}")
     public ApiSuccess<?> getMovieInfoById(@PathVariable("movieId") Long movieId) {
         AdminMovieTMDBDetailResponse data = TMDBService.getMovieInfoById(movieId);
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 데이터 생성", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(type = "string", example = "SUCCESS")))
+    })
     @PostMapping("/movie")
-    public ApiSuccess<?> createMovie(@RequestBody AdminMovieTMDBDetailResponse movie) {
+    public ApiSuccess<String> createMovie(@RequestBody AdminMovieTMDBDetailResponse movie) {
         adminMovieCreateService.createMovie(movie);
         return ApiUtil.success("SUCCESS");
     }
 
+    @Operation(summary = "영화 데이터 상세 조회", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(type = "string", example = "SUCCESS")))
+    })
     @GetMapping("/movie/{movieId}")
     public ApiSuccess<?> getMovieCategory(@PathVariable("movieId") Long movieId) {
         AdminMovieCategoryResponse data = adminMovieReadService.getMovieCategory(movieId);
