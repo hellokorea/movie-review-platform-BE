fa24e9d485230edbd1a46b5b730bf66aeec317fb
kjd <wjdehddk123@naver.com>
2024-12-05 15:59:46 +0900

    Feat: #91 Admin 도메인 Swagger 설정 완료

--- cookie/src/main/java/com/cookie/admin/controller/AdminMovieController.java
+++ cookie/src/main/java/com/cookie/admin/controller/AdminMovieController.java
@@ -7,8 +7,9 @@ import com.cookie.admin.service.movie.AdminMovieReadService;
 import com.cookie.admin.service.movie.AdminMovieSearchService;
 import com.cookie.global.util.ApiUtil;
 import com.cookie.global.util.ApiUtil.ApiSuccess;
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -18,9 +19,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Admin 영화 관리", description = "Admin 영화 관리 API")
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "Admin 영화 관리", description = "Admin 영화 관리 API")
 @RequestMapping("/api/admin")
 public class AdminMovieController {
 
@@ -30,7 +31,10 @@ public class AdminMovieController {
     private final AdminMovieModifyService adminMovieModifyService;
     private final AdminMovieSearchService adminMovieSearchService;
 
-    @Hidden
+    @Operation(summary = "영화 초기 세팅 값 - 4600편", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminMovieBaseAddResponse.class)))
+    })
     @PostMapping("/movie/base")
     public ApiSuccess<?> defaultMoviesAdd() {
         AdminMovieBaseAddResponse data = adminMovieCreateService.defaultMoviesAdd();
@@ -38,7 +42,8 @@ public class AdminMovieController {
     }
 
     @Operation(summary = "TMDB 영화 검색", responses = {
-            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdminSearchResponse.class)))
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminSearchResponse.class)))
     })
     @GetMapping("/movie/tmdb/{movieName}")
     public ApiSuccess<?> getMoviesByName(@PathVariable("movieName") String movieName) {
@@ -47,7 +52,8 @@ public class AdminMovieController {
     }
 
     @Operation(summary = "TMDB 검색 된 영화 선택", responses = {
-            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdminMovieTMDBDetailResponse.class)))
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminMovieTMDBDetailResponse.class)))
     })
     @GetMapping("/movie/tmdb/choice/{movieId}")
     public ApiSuccess<?> getMovieInfoById(@PathVariable("movieId") Long movieId) {
@@ -56,7 +62,8 @@ public class AdminMovieController {
     }
 
     @Operation(summary = "영화 데이터 생성", responses = {
-            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(type = "string", example = "SUCCESS")))
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(type = "string", example = "SUCCESS")))
     })
     @PostMapping("/movie")
     public ApiSuccess<String> createMovie(@RequestBody AdminMovieTMDBDetailResponse movie) {
@@ -64,8 +71,9 @@ public class AdminMovieController {
         return ApiUtil.success("SUCCESS");
     }
 
-    @Operation(summary = "영화 데이터 상세 조회", responses = {
-            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(type = "string", example = "SUCCESS")))
+    @Operation(summary = "영화 카테고리 조회", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminMovieCategoryResponse.class)))
     })
     @GetMapping("/movie/{movieId}")
     public ApiSuccess<?> getMovieCategory(@PathVariable("movieId") Long movieId) {
@@ -73,12 +81,25 @@ public class AdminMovieController {
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 데이터 상세 조회", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminMovieDetailResponse.class)))
+    })
     @GetMapping("/movie/{movieId}/detail")
     public ApiSuccess<?> getMovieDetail(@PathVariable("movieId") Long movieId) {
         AdminMovieDetailResponse data = adminMovieReadService.getMovieDetail(movieId);
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 카테고리 업데이트",
+            parameters = {
+                    @Parameter(name = "MovieCategories",
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = MovieCategories.class, nullable = true)))
+            }, responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminMovieCategoryResponse.class)))
+    })
     @PutMapping("/movie/{movieId}")
     public ApiSuccess<?> updateMovieCategory(@PathVariable("movieId") Long movieId,
                                              @RequestBody List<MovieCategories> categories) {
@@ -86,12 +107,20 @@ public class AdminMovieController {
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 데이터 삭제", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Long[].class)))
+            })
     @DeleteMapping("/movies")
     public ApiSuccess<?> deleteMovie(@RequestBody List<Long> movieIds) {
         List<Long> ids = adminMovieModifyService.deleteMovie(movieIds);
         return ApiUtil.success(ids);
     }
 
+    @Operation(summary = "영화 검색 Admin", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminMoviesResponse.class)))
+    })
     @GetMapping("/movies/{movieName}/{pageNumber}")
     public ApiSuccess<?> getMovieBySearch(@PathVariable("movieName") String  movieName,
                                           @PathVariable("pageNumber") Integer pageNumber) {
@@ -99,10 +128,13 @@ public class AdminMovieController {
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 전체 리스트 Admin", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminMoviesResponse.class)))
+    })
     @GetMapping("/movies/{pageNumber}")
     public ApiSuccess<?> getMovies(@PathVariable("pageNumber") Integer pageNumber) {
         AdminMoviesResponse data = adminMovieSearchService.getMovies(pageNumber);
         return ApiUtil.success(data);
     }
-
 }
--- cookie/src/main/java/com/cookie/admin/controller/AdminReviewController.java
+++ cookie/src/main/java/com/cookie/admin/controller/AdminReviewController.java
@@ -5,11 +5,18 @@ import com.cookie.admin.service.reviewAndLike.AdminReviewAndLikeService;
 import com.cookie.domain.review.service.ReviewService;
 import com.cookie.global.util.ApiUtil;
 import com.cookie.global.util.ApiUtil.ApiSuccess;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Admin 영화 리뷰 및 좋아요 관리", description = "Admin 영화 리뷰 및 좋아요 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
@@ -18,6 +25,11 @@ public class AdminReviewController {
     private final AdminReviewAndLikeService adminReviewAndLikeService;
     private final ReviewService reviewService;
 
+    @Operation(summary = "영화 리뷰 전체 리스트 With Query", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    array = @ArraySchema(
+                            schema = @Schema(implementation = AdminReviewResponse.class))))
+    })
     @GetMapping("/reviews/{movieId}")
     public ApiSuccess<?> getMovieReviews(@PathVariable("movieId") Long movieId,
                                          @RequestParam(value = "dateOrder", defaultValue = "latest") String dateOrder,
@@ -27,36 +39,62 @@ public class AdminReviewController {
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 리뷰 상세 정보", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminReviewDetailResponse.class)))
+    })
     @GetMapping("/reviews/detail/{reviewId}")
     public ApiSuccess<?> getMovieReviewsDetail(@PathVariable("reviewId") Long reviewId) {
         AdminReviewDetailResponse data = adminReviewAndLikeService.getMovieReviewsDetail(reviewId);
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 리뷰 삭제", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(type = "string", example = "SUCCESS")))
+    })
     @DeleteMapping("/reviews/detail/{reviewId}")
     public ApiSuccess<?> deleteReviewAdmin(@PathVariable("reviewId") Long reviewId) {
         reviewService.deleteReview(reviewId);
         return ApiUtil.success("SUCCESS");
     }
 
+    @Operation(summary = "영화 리뷰 댓글 리스트", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    array = @ArraySchema(
+                            schema = @Schema(implementation = AdminReviewDetailCommentsResponse.class))))
+    })
     @GetMapping("/reviews/detail/{reviewId}/comments")
     public ApiSuccess<?> getMovieReviewsDetailComments(@PathVariable("reviewId") Long reviewId) {
         List<AdminReviewDetailCommentsResponse> data = adminReviewAndLikeService.getMovieReviewsDetailComments(reviewId);
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 리뷰 댓글 삭제", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(type = "string", example = "SUCCESS")))
+    })
     @DeleteMapping("/reviews/detail/{commentId}/comments")
     public ApiSuccess<?> deleteReviewComment(@PathVariable("commentId") Long commentId) {
         reviewService.deleteComment(commentId);
         return ApiUtil.success("SUCCESS");
     }
 
+    @Operation(summary = "영화 리뷰 좋아요 리스트", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    array = @ArraySchema(
+                            schema = @Schema(implementation = AdminReviewDetailLikesResponse.class))))
+    })
     @GetMapping("/reviews/detail/{reviewId}/likes")
     public ApiSuccess<?> getMovieReviewsDetailLikes(@PathVariable("reviewId") Long reviewId) {
         List<AdminReviewDetailLikesResponse> data = adminReviewAndLikeService.getMovieReviewsDetailLikes(reviewId);
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 리뷰 숨기기", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminReviewHideResponse.class)))
+    })
     @PostMapping("/reviews/hide/{reviewId}")
     public ApiSuccess<?> updateReviewHideStatus(@PathVariable("reviewId") Long reviewId,
                                                 @RequestBody boolean hideStatus) {
@@ -64,6 +102,10 @@ public class AdminReviewController {
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 리뷰 스포일러 리뷰로 변경", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminReviewSpoilerResponse.class)))
+    })
     @PostMapping("/reviews/spoiler/{reviewId}")
     public ApiSuccess<?> updateReviewSpoilerStatus(@PathVariable("reviewId") Long reviewId,
                                                    @RequestBody boolean spoilerStatus) {
@@ -71,6 +113,10 @@ public class AdminReviewController {
         return ApiUtil.success(data);
     }
 
+    @Operation(summary = "영화 좋아요 리스트 With Query", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminMovieLikesResponse.class)))
+    })
     @GetMapping("/likes/{movieId}")
     public ApiSuccess<?> getMovieLikes(@PathVariable("movieId") Long movieId,
                                        @RequestParam(value = "dateOrder", defaultValue = "latest") String dateOrder) {
