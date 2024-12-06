3240a7b17f43dd25d641b544dfbd83be8f90f741
JungYujin <ujin8523@naver.com>
2024-12-06 17:05:44 +0900

    Refactor:#103 영화 상세보기에서 배우, 감독 id추가

--- cookie/src/main/java/com/cookie/domain/actor/dto/response/ActorResponse.java
+++ cookie/src/main/java/com/cookie/domain/actor/dto/response/ActorResponse.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ActorResponse {
+    private Long id;
     private String name;
     private String profileImage;
 }
