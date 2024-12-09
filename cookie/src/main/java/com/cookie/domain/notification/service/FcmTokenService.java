11806a3db8c8c7618d8d6fae96af1bb4d0db6f97
fbgjung <fbgjung@gmail.com>
2024-12-09 16:57:27 +0900

    Feat: #116 FCM 토큰 생성 및 삭제 API 추가

--- cookie/src/main/java/com/cookie/domain/notification/service/FcmTokenService.java
+++ cookie/src/main/java/com/cookie/domain/notification/service/FcmTokenService.java
@@ -14,8 +14,9 @@ import org.springframework.stereotype.Service;
 public class FcmTokenService {
     private final UserRepository userRepository;
     private final FcmTokenRepository fcmTokenRepository;
+    private final NotificationService notificationService;
 
-    public void saveToken(Long userId, String token) {
+    public void saveFcmToken(Long userId, String token) {
         log.info("saveToken userId: {} and token: {}", userId, token);
 
         User user = userRepository.findById(userId)
@@ -27,7 +28,23 @@ public class FcmTokenService {
                 .build();
 
         fcmTokenRepository.save(fcmToken);
+        log.info("Saved fcm token");
+//        notificationService.subscribeToTopic(fcmToken.getToken(), user.getCategory().getId(), userId); // 구독 취소
 
+    }
+
+    public void deleteFcmToken(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("not found userId: " + userId));
+
+        FcmToken fcmToken = fcmTokenRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException("not found fcmToken"));
+
+        fcmTokenRepository.delete(fcmToken);
+        log.info("Deleted fcm token");
+
+//        notificationService.unsubscribeFromTopic(fcmToken.getToken(), user.getCategory().getId(), userId); // 구독 취소
+//        log.info("Unsubscribe Topic");
     }
 
 }
