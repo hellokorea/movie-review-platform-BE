7dd46950b1016ed37f4359c80add19a29d54c485
fbgjung <fbgjung@gmail.com>
2024-12-12 20:23:03 +0900

    Feat: #122 알림 읽음처리 및 리뷰 상세보기 페이지 이동을 위한 요청 필드 추가

--- cookie/src/main/java/com/cookie/domain/notification/dto/request/NotificationRequest.java
+++ cookie/src/main/java/com/cookie/domain/notification/dto/request/NotificationRequest.java
@@ -1,17 +1,32 @@
 package com.cookie.domain.notification.dto.request;
 
 
-import lombok.AllArgsConstructor;
+import com.cookie.domain.notification.entity.enums.Status;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class NotificationRequest {
+    private String notificationId;
     private String body;
     private String senderProfileImage;
     private LocalDateTime timestamp;
+    private Long reviewId;
+    @Setter
+    private Status status;
+
+    @Builder
+    public NotificationRequest(String notificationId, String body, String senderProfileImage, LocalDateTime timestamp, Long reviewId, Status status) {
+        this.notificationId = notificationId;
+        this.body = body;
+        this.senderProfileImage = senderProfileImage;
+        this.timestamp = timestamp;
+        this.reviewId = reviewId;
+        this.status = status;
+    }
 }
