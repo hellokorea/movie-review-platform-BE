236ac9974988611e9a296d6f381e59d424bc30c4
fbgjung <fbgjung@gmail.com>
2024-12-13 02:25:52 +0900

    Hotfix: 병합 후 오류 수정

--- cookie/src/main/java/com/cookie/global/config/WebSocketConfig.java
+++ cookie/src/main/java/com/cookie/global/config/WebSocketConfig.java
@@ -24,6 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("https://cookiekie.com").withSockJS();
+//        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173").withSockJS();
     }
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
