b1aa37a016fa2a5c7c70d4c7def7528209f6ddbb
fbgjung <fbgjung@gmail.com>
2024-12-12 20:29:00 +0900

    Feat: #122 푸쉬 알림 목록 및 읽음처리 API 구현

--- cookie/src/main/java/com/cookie/domain/notification/controller/NotificationController.java
+++ cookie/src/main/java/com/cookie/domain/notification/controller/NotificationController.java
@@ -1,7 +1,10 @@
 package com.cookie.domain.notification.controller;
 
 import com.cookie.domain.notification.dto.request.FcmTokenRequest;
+import com.cookie.domain.notification.dto.request.NotificationReadRequest;
+import com.cookie.domain.notification.dto.response.NotificationResponse;
 import com.cookie.domain.notification.service.FcmTokenService;
+import com.cookie.domain.notification.service.NotificationService;
 import com.cookie.domain.user.dto.response.auth.CustomOAuth2User;
 import com.cookie.domain.user.service.UserService;
 import com.cookie.global.util.ApiUtil;
@@ -10,18 +13,23 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import java.util.List;
+
+@Slf4j
 @Tag(name = "푸쉬 알림", description = "푸쉬 알림 설정 및 FCM 토큰 관리 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/notification/")
+@RequestMapping("/api/notification")
 public class NotificationController {
 
     private final FcmTokenService fcmTokenService;
     private final UserService userService;
+    private final NotificationService notificationService;
 
     @Operation(summary = "FCM 토큰 저장", description = "사용자의 FCM 토큰을 저장합니다. 푸쉬 알림이 활성화된 사용자만 저장 가능합니다.",
             responses = {
@@ -50,6 +58,8 @@ public class NotificationController {
         return ApiUtil.success("SUCCESS");
     }
 
+
+
     @Operation(summary = "푸쉬 알림 설정 변경", description = "사용자의 푸쉬 알림을 설정합니다. 알림이 활성화되면 FCM 토큰이 저장되고, 비활성화되면 FCM 토큰이 삭제됩니다.",
             responses = {
                     @ApiResponse(responseCode = "200", description = "푸쉬 알림 설정 변경 성공", content = @Content(mediaType = "application/json")),
@@ -61,6 +71,36 @@ public class NotificationController {
     public ApiSuccess<?> toggleNotification(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestBody FcmTokenRequest fcmTokenRequest) {
         Long userId = customOAuth2User.getId();
         userService.togglePushNotification(userId, fcmTokenRequest);
+        return ApiUtil.success("SUCCESS");
+    }
+
+
+    @Operation(summary = "사용자의 알림 목록 조회", description = "사용자의 모든 알림을 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "알림 목록 조회 성공", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음", content = @Content(mediaType = "application/json"))
+            }
+    )
+    @GetMapping
+    public ApiSuccess<?> getNotifications(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        Long userId = customOAuth2User.getId();
+        List<NotificationResponse> notifications = notificationService.getNotifications(userId);
+        return ApiUtil.success(notifications);
+    }
+
+
+    @Operation(summary = "알림 읽음 처리", description = "특정 알림을 읽음 상태로 처리합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "알림 읽음 처리 성공", content = @Content(mediaType = "application/json")),
+            }
+    )
+    @PostMapping("/read-status")
+    public ApiSuccess<?> markNotificationAsRead(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestBody NotificationReadRequest notificationReadRequest) {
+        Long userId = customOAuth2User.getId();
+        String notificationId = notificationReadRequest.getNotificationId();
+
+        notificationService.markNotificationAsRead(userId, notificationId);
+
         return ApiUtil.success("SUCCESS");
     }
 
--- cookie/src/main/java/com/cookie/domain/notification/service/NotificationService.java
+++ cookie/src/main/java/com/cookie/domain/notification/service/NotificationService.java
@@ -1,18 +1,28 @@
 package com.cookie.domain.notification.service;
 
 
-import com.cookie.domain.category.repository.CategoryRepository;
-import com.cookie.domain.category.entity.Category;
+import com.cookie.domain.notification.dto.request.NotificationRequest;
+import com.cookie.domain.notification.dto.response.NotificationResponse;
+import com.cookie.domain.notification.entity.FcmToken;
+import com.cookie.domain.notification.entity.enums.Status;
+import com.cookie.domain.notification.repository.FcmTokenRepository;
+import com.cookie.domain.user.entity.User;
 import com.cookie.domain.user.repository.UserRepository;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -20,87 +30,130 @@ import java.util.List;
 public class NotificationService {
 
     private final UserRepository userRepository;
-    private final CategoryRepository categoryRepository;
+    private final FcmTokenRepository fcmTokenRepository;
     private final FirebaseMessaging firebaseMessaging;
+    private final RedisTemplate<String, Object> template;
+    private final ObjectMapper objectMapper;
+    private static final String NOTIFICATION_KEY = "notifications:user:";
 
-    public void subscribeToTopic(String token, Long categoryId, Long userId) {
+    /**
+     * 푸쉬알림 전송
+     */
+    @Async
+    public void sendPushNotificationToUsers(Long senderId, List<String> tokens, String title, String body, Long reviewId) {
+        User sender = userRepository.findById(senderId)
+                .orElseThrow(() -> new IllegalArgumentException("not found userId"));
+        String senderProfileImage = sender.getProfileImage();
 
-        userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("not found userId: " + userId));
-
-        Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new IllegalArgumentException("not found categoryId: " + categoryId));
-
-        String topic = category.getSubCategoryEn();
-
-        try {
-            FirebaseMessaging.getInstance().subscribeToTopic(List.of(token), topic);
-            log.info("Subscribed to topic: {}", topic);
-        } catch (FirebaseMessagingException e) {
-            log.error("Failed subscribe to topic: {}", e.getMessage());
-
-        }
-
-    }
-
-    public void unsubscribeFromTopic(String token, Long categoryId, Long userId) {
-        userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("not found userId: " + userId));
-
-        Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new IllegalArgumentException("not found categoryId: " + categoryId));
-
-        String topic = category.getSubCategoryEn();
-
-        try {
-            FirebaseMessaging.getInstance().unsubscribeFromTopic(List.of(token), topic);
-            log.info("Unsubscribed from topic: {}", topic);
-        } catch (FirebaseMessagingException e) {
-            log.error("Failed to unsubscribe from topic: {}", e.getMessage());
-        }
-    }
-
-
-    public void sendPushNotificationToTopic(String topic, String title, String body) {
-        Message message = Message.builder()
-                .setNotification(Notification.builder()
-                        .setTitle(title)
-                        .setBody(body)
-                        .build())
-                .setTopic(topic)
-                .build();
-
-        try {
-            String response = firebaseMessaging.send(message);
-            log.info("Send push notification: {}", response);
-        } catch (FirebaseMessagingException e) {
-            log.error("Error sending push notification: {}", e.getMessage());
-        }
-    }
-
-
-    public void sendPushNotificationToUsers(List<String> tokens, String title, String body) {
         for (String token : tokens) {
-            // 작성자의 토큰은 제외
-//            if (excludedTokens.contains(token)) {
-//                continue;
-//            }
+
+            FcmToken fcmToken = fcmTokenRepository.findByToken(token)
+                    .orElseThrow(() -> new IllegalArgumentException("not found token"));
+            Long userId = fcmToken.getUser().getId(); // 받는 사람 id
+
 
             Message message = Message.builder()
-                    .setToken(token)
-                    .setNotification(Notification.builder()
-                            .setTitle(title)
-                            .setBody(body)
+                    .setWebpushConfig(WebpushConfig.builder()
+                            .putHeader("Urgency", "high")
                             .build())
+                    .putData("title", title)
+                    .putData("body", body)
+                    .setToken(token)
                     .build();
 
             try {
-                String response = firebaseMessaging.send(message);
-                log.info("Push notification sent successfully to token: {}", token);
+                firebaseMessaging.send(message); // 푸쉬 알림 전송
+                log.info("푸쉬 알림 전송 성공: token {}", token);
+                saveUserNotificationToRedis(userId, body, senderProfileImage, reviewId); // 알림 저장
             } catch (FirebaseMessagingException e) {
-                log.error("Error sending push notification to token {}: {}", token, e.getMessage());
+                log.error("푸쉬 알림 전송 실패: token {}, error: {}", token, e.getMessage());
             }
         }
     }
 
+
+    /*
+     * 사용자 별 알림 리스트 조회
+     */
+    @Transactional(readOnly = true)
+    public List<NotificationResponse> getNotifications(Long userId) {
+        String key = NOTIFICATION_KEY + userId;
+        List<Object> notifications = template.opsForList().range(key, 0, -1);
+
+        if (notifications == null) {
+            log.warn("알림이 존재하지 않습니다. userId: {}", userId);
+            return new ArrayList<>();
+        }
+
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        List<NotificationResponse> result = new ArrayList<>();
+
+        for (Object notification : notifications) {
+            try {
+                NotificationResponse notificationResponse = objectMapper.readValue(notification.toString(), NotificationResponse.class);
+                result.add(notificationResponse);
+            } catch (Exception e) {
+                log.error("Error parsing notification JSON: {}", notification, e);
+            }
+        }
+
+        return result;
+    }
+
+
+    /*
+     * redis 알림 저장
+     */
+    @Async
+    public void saveUserNotificationToRedis(Long userId, String body, String senderProfileImage, Long reviewId) {
+        String key = NOTIFICATION_KEY + userId;
+
+        NotificationRequest notificationRequest = NotificationRequest.builder()
+                .notificationId(UUID.randomUUID().toString())
+                .body(body)
+                .senderProfileImage(senderProfileImage)
+                .timestamp(LocalDateTime.now())
+                .reviewId(reviewId)
+                .status(Status.UNREAD)
+                .build();
+
+        try {
+            template.opsForList().leftPush(key, objectMapper.writeValueAsString(notificationRequest));
+            log.info("알림 저장 성공 userId: {}", userId);
+        } catch (Exception e) {
+            log.error("Error saving notification to Redis: {}", e.getMessage());
+        }
+    }
+
+
+    /*
+     * 읽음 처리 갱신
+     */
+    public void markNotificationAsRead(Long userId, String notificationId) {
+        String key = NOTIFICATION_KEY + userId;
+
+        List<Object> notifications = template.opsForList().range(key, 0, -1);
+        if (notifications == null || notifications.isEmpty()) {
+            log.warn("알림이 존재하지 않습니다. userId: {}", userId);
+            return;
+        }
+
+        for (int i = 0; i < notifications.size(); i++) {
+            try {
+                NotificationRequest notificationRequest = objectMapper.readValue(notifications.get(i).toString(), NotificationRequest.class);
+
+                if (notificationRequest.getNotificationId().equals(notificationId)) {
+                    notificationRequest.setStatus(Status.READ);
+                    template.opsForList().set(key, i, objectMapper.writeValueAsString(notificationRequest));
+                    log.info("알림 읽음 처리 완료: userId {}, notificationId {}", userId, notificationId);
+                    return;
+                }
+            } catch (Exception e) {
+                log.error("Error parsing or updating notification: {}", notifications.get(i), e);
+            }
+        }
+
+        log.warn("일치하는 알림을 찾을 수 없습니다 userId: {} and notificationId: {}", userId, notificationId);
+    }
 }
+
